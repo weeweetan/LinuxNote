b537c4d4d409b8774a024696239e24b722416435
wulikanhua <1113513062@qq.com>
2017-11-30 16:38:44 +0800

    for...in

---
+++ bash_shell/pingip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+network="192.168.1"
+
+for sitenu in $(seq 1 100)
+do
+    ping -c 1 -w 1 ${network}.${sitenu} &>/dev/null && result=0 || result=1
+    if [ "${result}" == 0 ]; then
+        echo "Server ${network}.${sitenu} is UP"
+    else 
+        echo "Server ${network}.${sitenu} is DOWN"
+    fi
+done
+
+
